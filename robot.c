fc8b2cc4f9cebba41adbb8862710cb30eedb85a0
Diego E <diegoeche@gmail.com>
2017-10-22 16:14:46 +0000

    More granular steps

--- robot.c
+++ robot.c
@@ -19,6 +19,7 @@ int s, i, slen = sizeof(si_other), recv_len;
 char buf[BUFLEN];
 
 typedef enum robot_state_t{
+  STARTING,
   STOP,
   UP,
   DOWN,
@@ -47,6 +48,12 @@ void sig_handler(int signo)
   }
 }
 
+void sendCommand(robot_state_t comm) {
+  pthread_mutex_lock( &mutex1 );
+  command = comm;
+  pthread_mutex_unlock( &mutex1 );
+}
+
 void UDPLog(const char* format, ...) {
     char       msg[100];
     va_list    args;
@@ -56,17 +63,6 @@ void UDPLog(const char* format, ...) {
     va_end(args);
 
     printf(" [UDP Server] %s\n", msg);
-}
-
-void ControlLog(const char* format, ...) {
-    char       msg[100];
-    va_list    args;
-
-    va_start(args, format);
-    vsnprintf(msg, sizeof(msg), format, args); // do check return value
-    va_end(args);
-
-    printf(" [Control] %s\n", msg);
 }
 
 void *UDPServer() {
@@ -97,33 +93,25 @@ void *UDPServer() {
     }
 
     UDPLog("Received packet from %s:%d", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-    UDPLog("Data:\n[%s]", buf);
+    UDPLog("Data: [%s  ]", buf);
 
     if(strcmp(buf, "UP\n") == 0) {
-      pthread_mutex_lock( &mutex1 );
-      command = UP;
-      pthread_mutex_unlock( &mutex1 );
+      sendCommand(UP);
       UDPLog("UP Command Sent");
     }
 
     if(strcmp(buf, "DOWN\n") == 0) {
-      pthread_mutex_lock( &mutex1 );
-      command = DOWN;
-      pthread_mutex_unlock( &mutex1 );
+      sendCommand(DOWN);
       UDPLog("DOWN Command Sent");
     }
 
     if(strcmp(buf, "STOP\n") == 0) {
-      pthread_mutex_lock( &mutex1 );
-      command = STOP;
-      pthread_mutex_unlock( &mutex1 );
+      sendCommand(STOP);
       UDPLog("STOP Command Sent");
     }
 
     if(strcmp(buf, "KILL\n") == 0) {
-      pthread_mutex_lock( &mutex1 );
-      command = KILL;
-      pthread_mutex_unlock( &mutex1 );
+      sendCommand(KILL);
       UDPLog("KILL Command Sent");
     }
 
@@ -156,9 +144,19 @@ void use_both_motors(float duty) {
   rc_set_motor(2, speed_two);
 }
 
+void ControlLog(const char* format, ...) {
+    char       msg[100];
+    va_list    args;
+
+    va_start(args, format);
+    vsnprintf(msg, sizeof(msg), format, args); // do check return value
+    va_end(args);
+
+    printf(" [Control] %s\n", msg);
+}
 
 void *Control() {
-  float duty = 0.6;
+  float duty = 0.8;
   int time = 1;
 
   // Sanity check cape library initialized
@@ -176,32 +174,36 @@ void *Control() {
 
   ControlLog("Motors activated. Starting Control Loop.");
 
+  robot_state_t last_command = STARTING;
+
   while(1) {
     pthread_mutex_lock( &mutex1 );
     robot_state_t read_command = command;
     pthread_mutex_unlock( &mutex1 );
-
-    switch(read_command) {
-    case STOP:
-      rc_set_motor_brake(1);
-      rc_set_motor_brake(2);
-      ControlLog("Brakes 1,2 activated");
-      break;
-    case UP:
-      use_both_motors(duty);
-      ControlLog("Going forward at: [%0.4f]", duty);
-      break;
-    case DOWN:
-      use_both_motors(-duty);
-      ControlLog("Going backwards at: [%0.4f]", duty);
-      break;
-    case KILL:
-      ControlLog("Ending 'Control' Thread");
-      return;
-    default:
-      ControlLog("Message not implemented");
+    if(read_command != last_command) {
+      switch(read_command) {
+      case STOP:
+	rc_set_motor_brake(1);
+	rc_set_motor_brake(2);
+	ControlLog("Brakes 1,2 activated");
+	break;
+      case UP:
+	use_both_motors(duty);
+	ControlLog("Going forward at: [%0.4f]", duty);
+	break;
+      case DOWN:
+	use_both_motors(-duty);
+	ControlLog("Going backwards at: [%0.4f]", duty);
+	break;
+      case KILL:
+	ControlLog("Ending 'Control' Thread");
+	return;
+      default:
+	ControlLog("Message not implemented");
+      }
+      last_command = read_command;
     }
-    sleep(time);
+    usleep(100 * 1000);
   }
 }
 
